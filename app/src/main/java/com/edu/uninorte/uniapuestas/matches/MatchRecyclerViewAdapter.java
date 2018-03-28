b9b02cec5beb03a68fb9457a8e1e0d7903ec60c0
erwinb1996@gmail.com <erwinb1996@gmail.com>
2018-03-28 14:49:39 -0500

    added alert dialog functionally on match recycler view adapter

--- app/src/main/java/com/edu/uninorte/uniapuestas/matches/MatchRecyclerViewAdapter.java
+++ app/src/main/java/com/edu/uninorte/uniapuestas/matches/MatchRecyclerViewAdapter.java
@@ -1,9 +1,13 @@
 package com.edu.uninorte.uniapuestas.matches;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.edu.uninorte.uniapuestas.R;
@@ -33,6 +37,37 @@ public class MatchRecyclerViewAdapter extends RecyclerView.Adapter<MatchRecycler
         holder.textoNombrePartido.setText(matchesData.get(position).getHomeTeam()+" VS "+ matchesData.get(position).getAwayTeam()+"");
         holder.textoFecha.setText(matchesData.get(position).getDate()+"");
         holder.textoFase.setText("Fase de grupos");
+
+        holder.btnApostar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setTitle("Cambiar termino");
+                View dialog = LayoutInflater.from(view.getContext()).inflate(R.layout.bet_form, null, false);
+                TextView textoHome = dialog.findViewById(R.id.textoHome);
+                TextView textoAway = dialog.findViewById(R.id.textoAway);
+
+                textoHome.setText(matchesData.get(position).getHomeTeam());
+                textoAway.setText(matchesData.get(position).getAwayTeam());
+
+
+                builder.setView(dialog);
+                builder.setPositiveButton("Apostar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.setCancelable(false);
+                builder.show();
+            }
+        });
         //holder.textoVotosTeamA.setText("Gana equipo A: "+matchesData.get(position).getUsersTeamA()+" Usuarios");
         //holder.textoEmpates.setText("Empate: "+matchesData.get(position).getUsersDraw()+"Usuarios");
         //holder.textoVotosTeamB.setText("Gana equipo B: "+matchesData.get(position).getUsersTeamB()+"Usuarios");
@@ -52,6 +87,7 @@ public class MatchRecyclerViewAdapter extends RecyclerView.Adapter<MatchRecycler
         TextView textoVotosTeamA;
         TextView textoEmpates;
         TextView textoVotosTeamB;
+        Button btnApostar;
 
         public ViewHolder(View itemView){
             super(itemView);
@@ -61,6 +97,11 @@ public class MatchRecyclerViewAdapter extends RecyclerView.Adapter<MatchRecycler
             textoVotosTeamA = itemView.findViewById(R.id.textoVotosTeamA);
             textoEmpates = itemView.findViewById(R.id.textoEmpates);
             textoVotosTeamB = itemView.findViewById(R.id.textoVotosTeamB);
+            btnApostar = itemView.findViewById(R.id.btnApostar);
+        }
+
+        public Button getBtnApostar() {
+            return btnApostar;
         }
     }
 
